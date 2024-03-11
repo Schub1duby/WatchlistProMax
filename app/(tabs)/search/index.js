c68aad89cad1fe826cb1cf00cd3cb03919ed5348
Cyrill Schudel <cyrill.schudel@raiffeisen.ch>
2024-03-11 09:01:41 +0100

    feat: searchbar works

--- app/(tabs)/search/index.js
+++ app/(tabs)/search/index.js
@@ -1,18 +1,22 @@
-import {View, Text, StyleSheet, FlatList, Alert} from "react-native"
+import {View, Text, StyleSheet, FlatList, Alert, TextInput} from "react-native"
 import React, {useEffect, useState} from "react";
 import WatchListItem from "../../../components/WatchListItem";
+import SearchInput from "../../../components/SearchInput";
 
 export default function HomeScreen() {
     let [movies, setMovies] = useState([])
     let [isLoading, setIsLoading] = useState(true)
+    let [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-        const loadMovies = async () => {
+        const loadMovies = async (query) => {
+            if (query === undefined || query === "") {
+                return null
+            }
             try {
-                const response = await fetch("https://www.omdbapi.com/?s=iron&page=1&apikey=e69bd20e", {
+                const response = await fetch(`https://www.omdbapi.com/?s=${query}&page=1&apikey=e69bd20e`, {
                     method: 'GET',
                     headers: {
-                        contentType: "application/json"
+                        "Content-Type": "application/json"
                     }
                 })
                 if (!response.ok) {
@@ -30,21 +34,26 @@ export default function HomeScreen() {
                 Alert.alert("An Error occurred", error.message)
             }
         }
-        loadMovies()
-    }, [])
 
     //console.log(movies)
 
 
     return (
-        isLoading ?
-            <Text>Loading</Text>
-            :
-            <FlatList data={movies}
-                      renderItem={({item}) => <WatchListItem item={item} />}
-                      keyExtractor={item => item.imdbID}
+            <View>
+                <SearchInput placeholder={"Search for movies ..."} value={searchQuery}
+                onSearchPress={(query=> {
+                    setSearchQuery(query);
+                    loadMovies(query);
+                })}>
 
-            />
+                </SearchInput>
+                <FlatList data={movies}
+                          renderItem={({item}) => <WatchListItem item={item} />}
+                          keyExtractor={item => item.imdbID}
+
+                />
+            </View>
+
 
     )
 }
@@ -54,5 +63,14 @@ const defaultStyle = StyleSheet.create({
         display: "flex",
         justifyContent: "center",
         alignContent: "center"
-    }
+    },
+    Input: {
+        height: 48,
+        marginTop: 5,
+        marginBottom: 5,
+        fontSize: 24,
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 10,
+    },
 })
