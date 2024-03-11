a0f85bb164e7ee98c8fa26991f76ae6ba2da866d
Cyrill Schudel <cyrill.schudel@raiffeisen.ch>
2024-03-11 09:04:16 +0100

    refactor: rearragen code

--- app/(tabs)/search/index.js
+++ app/(tabs)/search/index.js
@@ -8,51 +8,49 @@ export default function HomeScreen() {
     let [isLoading, setIsLoading] = useState(true)
     let [searchQuery, setSearchQuery] = useState('');
 
-        const loadMovies = async (query) => {
-            if (query === undefined || query === "") {
-                return null
-            }
-            try {
-                const response = await fetch(`https://www.omdbapi.com/?s=${query}&page=1&apikey=e69bd20e`, {
-                    method: 'GET',
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                })
-                if (!response.ok) {
-                    Alert.alert("Error", `The Server responded with ${response.status} ${response.statusText}`)
-                }
-                const movieData = await response.json()
-                if (movieData.Response === "True") {
-                    setMovies(movieData.Search)
-                } else {
-                    Alert.alert("Error", "No Movies found")
-                }
-                setIsLoading(false)
-            } catch (error) {
-                console.log(error)
-                Alert.alert("An Error occurred", error.message)
-            }
+    const loadMovies = async (query) => {
+        if (query === undefined || query === "") {
+            return null
         }
-
-    //console.log(movies)
+        try {
+            const response = await fetch(`https://www.omdbapi.com/?s=${query}&page=1&apikey=e69bd20e`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            if (!response.ok) {
+                Alert.alert("Error", `The Server responded with ${response.status} ${response.statusText}`)
+            }
+            const movieData = await response.json()
+            if (movieData.Response === "True") {
+                setMovies(movieData.Search)
+            } else {
+                Alert.alert("Error", "No Movies found")
+            }
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("An Error occurred", error.message)
+        }
+    }
 
 
     return (
-            <View>
-                <SearchInput placeholder={"Search for movies ..."} value={searchQuery}
-                onSearchPress={(query=> {
-                    setSearchQuery(query);
-                    loadMovies(query);
-                })}>
+        <View>
+            <SearchInput placeholder={"Search for movies ..."} value={searchQuery}
+                         onSearchPress={(query => {
+                             setSearchQuery(query);
+                             loadMovies(query);
+                         })}>
 
-                </SearchInput>
-                <FlatList data={movies}
-                          renderItem={({item}) => <WatchListItem item={item} />}
-                          keyExtractor={item => item.imdbID}
+            </SearchInput>
+            <FlatList data={movies}
+                      renderItem={({item}) => <WatchListItem item={item}/>}
+                      keyExtractor={item => item.imdbID}
 
-                />
-            </View>
+            />
+        </View>
 
 
     )
